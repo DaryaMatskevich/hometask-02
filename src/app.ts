68b10e85d3223baf20ac28cf4e042e97dc4f70e0
Darya Matskevich <darramatskevich@gmail.com>
2025-01-18 23:34:14 +0300

    changes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express'
+import { blogsRouter } from './Controllers/blogsController'
+import { postsRouter } from './Controllers/postsController'
 
 export const app = express()
 app.use(express.json())
+app.use('/blogs', blogsRouter)
+app.use('/posts', postsRouter)
+
+
 
 app.get('/', (req, res) => {
     res.status(200).json({version: '1.0'})
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
 import { app } from "./app";
-import { blogsRouter } from "./Controllers/blogsController";
-import { postsRouter } from "./Controllers/postsController";
 import { SETTINGS } from "./settings";
 
-
-app.use('/blogs', blogsRouter)
-app.use('/posts', postsRouter)
 app.listen(SETTINGS.PORT, () => {
     console.log('...server started in port ' + SETTINGS.PORT)
 })
