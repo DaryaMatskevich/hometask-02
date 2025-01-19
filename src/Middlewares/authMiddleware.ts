d445f350cea0d5167113b0397ef12d4cdc9b07a2
Darya Matskevich <darramatskevich@gmail.com>
2025-01-19 15:05:52 +0300

    changes

--- src/Middlewares/authMiddleware.ts
+++ src/Middlewares/authMiddleware.ts
@@ -3,14 +3,14 @@ import { ADMIN_AUTH } from '../settings.js'
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) =>  {
 const auth = req.headers['authorization'] as string;
 if (!auth) {
-    res.sendStatus(401);
+    res.status(401).json({});
     return;
 }
 const buff = Buffer.from(auth.slice(6), 'base64')
 const decodeAuth = buff.toString('utf8')
 
 if (decodeAuth !== ADMIN_AUTH && auth.slice(0,5) !== 'Basic') {
-    res.sendStatus(401)
+    res.status(401).json({})
     return;
 }
 next()
