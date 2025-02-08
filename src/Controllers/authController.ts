847aa657d545b69c9f78884ac2b8abdb62b697d0
Darya Matskevich <darramatskevich@gmail.com>
2025-02-09 00:35:45 +0300

    changes

--- src/Controllers/authController.ts
+++ src/Controllers/authController.ts
@@ -1,13 +1,12 @@
 import { Request, Response, Router } from "express";
 import { loginValidation, passwordValidation, emailValidation} from "../Middlewares/middlewares";
-import {authMiddleware } from "../Middlewares/authMiddleware"
 import { usersQueryRepository } from "../queryRepository/usersQueryRepository";
 export const authRouter = Router({})
 
-authRouter.post('/', authMiddleware, loginValidation, emailValidation, passwordValidation, async (req: Request, res: Response): Promise<any> => {
+authRouter.post('/', loginValidation, emailValidation, passwordValidation, async (req: Request, res: Response): Promise<any> => {
     const {loginOrEmail, password} = req.body;
      const user = await usersQueryRepository.findUserByLoginOrEmailforAuth(loginOrEmail) 
-    if (user) {
+    if (user.password === password) {
      return res.sendStatus(204)}
      else {
         return res.sendStatus(401)
