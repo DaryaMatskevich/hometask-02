91235cbded0fe1a36ccaf9f04eb640f0f586c1a9
Darya Matskevich <darramatskevich@gmail.com>
2025-02-08 21:45:13 +0300

    changes

--- src/queryRepository/usersQueryRepository.ts
+++ src/queryRepository/usersQueryRepository.ts
@@ -14,36 +14,49 @@ export const usersQueryRepository = {
         if (searchLoginTerm || searchEmailTerm) {
             filter.$or = [];
             if (searchLoginTerm) {
-            filter.$or.push({login : { $regex: searchLoginTerm, $options: 'i' }})
+                filter.$or.push({ login: { $regex: searchLoginTerm, $options: 'i' } })
             }
             if (searchEmailTerm) {
-                filter.$or.push({email : { $regex: searchEmailTerm, $options: 'i' }})
-            }}
-            
-            const result = await usersCollection.find(filter).sort({ [sortBy]: sortDirection === 'asc' ? 1 : -1 })
-                .skip((pageNumber - 1) * pageSize)
-                .limit(pageSize)
-                .toArray();
-            return result.map(user => ({
-                    id: user._id.toString(),
-                    login: user.login,
-                    password: user.password,
-                    email: user.email,
-                    createdAt: user.createdAt
-                }))
-            
+                filter.$or.push({ email: { $regex: searchEmailTerm, $options: 'i' } })
+            }
+        }
+        const users = await usersCollection.find(filter).sort({ [sortBy]: sortDirection === 'asc' ? 1 : -1 })
+            .skip((pageNumber - 1) * pageSize)
+            .limit(pageSize)
+            .toArray();
+        const mappedUsers = users.map(user => ({
+            id: user._id.toString(),
+            login: user.login,
+            password: user.password,
+            email: user.email,
+            createdAt: user.createdAt
+        }))
+        const usersCount: any = usersCollection.countDocuments(filter)
+
+        return {
+            pagesCount: Math.ceil(usersCount / pageSize),
+            page: pageNumber,
+            pageSize: pageSize,
+            totalCount: usersCount,
+            items: mappedUsers
+        }
+
     },
+
+
 
 
     async findUserById(id: string) {
         let user: any | null = await usersCollection.findOne({ _id: new ObjectId(id) });
         if (user) {
-            user.id = user._id
-            delete user._id
-            return user
+            return {
+                id: user._id.toString(),
+                login: user.login,
+                email: user.email,
+                createdAt: user.createdAt
+            }
         } else {
             return null
         }
-
     }
 }
