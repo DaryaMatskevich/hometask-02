d55a50a18fb128b3d0767aa95e262ace966a3d4d
Darya Matskevich <darramatskevich@gmail.com>
2025-02-25 09:58:24 +0300

    changes

--- src/Controllers/authController.ts
+++ src/Controllers/authController.ts
@@ -24,7 +24,8 @@ authRouter.get('/me', userAuthMiddleware, async (req: Request, res: Response) =>
 }
 )
 
-authRouter.post('/registration', loginValidation, emailValidation, passwordValidation, async (req: Request, res: Response) => {
+authRouter.post('/registration', loginValidation, emailValidation, passwordValidation,
+   inputValidationMiddleware, async (req: Request, res: Response) => {
 const user = await usersService.createUser(req.body.login, req.body.password, req.body.email)
 if (user.errorsMessages) {
    res.status(400).json({ errorsMessages: user.errorsMessages })
