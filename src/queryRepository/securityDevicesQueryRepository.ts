17e3f3ed4ed2ee23ec0a85a9e5ae37fb47a387cf
Darya Matskevich <darramatskevich@gmail.com>
2025-03-13 19:29:15 +0300

    changes

--- src/queryRepository/securityDevicesQueryRepository.ts
+++ src/queryRepository/securityDevicesQueryRepository.ts
@@ -3,6 +3,11 @@ import { devicesCollection } from "../Repository/db"
 export const securityDevicesQueryRepository = {
     async findSecurityDevices(userId: string) {
         const result = await devicesCollection.find({userId: new Object(userId)}).toArray()
-        return result
+        return result.map(device=> ({
+            ip: device.ip,
+            title: device.title,
+            lastActiveDate: device.iat,
+            deviceId: device.deviceId
+        }))
     }
 }
