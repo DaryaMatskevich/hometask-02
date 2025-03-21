8ab04d2e16fd54e113350085b46c6e82f6327e92
Darya Matskevich <darramatskevich@gmail.com>
2025-03-21 14:18:20 +0300

    changes

--- src/Middlewares/requestCountMiddleware.ts
+++ src/Middlewares/requestCountMiddleware.ts
@@ -2,28 +2,30 @@ import { NextFunction, Request, Response } from "express"
 import { apiRequestCountCollection } from "../Repository/db";
 
 export const requestCountMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-   const IP = req.ip;
-   const URL = req.originalUrl;
-   const currentTime = new Date();
-const timeLimit = new Date(currentTime.getTime() - 10000)
+    const IP = req.ip;
+    const URL = req.originalUrl;
+    const currentTime = new Date();
+    const timeLimit = new Date(currentTime.getTime() - 10000)
+
+    await apiRequestCountCollection.deleteMany({
+        date: { $lt: timeLimit },
+    });
+
+    await apiRequestCountCollection.insertOne({ IP, URL, date: currentTime })
+
+    const requestCount = await apiRequestCountCollection.countDocuments({
+        IP,
+        URL,
+        date: { $gte: timeLimit }
+    })
 
 
 
-   await apiRequestCountCollection.insertOne({IP, URL, date: currentTime})
-
-   const requestCount = await apiRequestCountCollection.countDocuments({
-    IP,
-    URL, 
-    date: {$gte: timeLimit} 
-   })
-
-
-
-if(requestCount > 5)  {
-    res.sendStatus(429)
-    return
-}
-else {
-    next()
-}
+    if (requestCount > 5) {
+        res.sendStatus(429)
+        return
+    }
+    else {
+        next()
+    }
 }
