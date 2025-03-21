938097988eff2b354430ac0a386c966cc4001aee
Darya Matskevich <darramatskevich@gmail.com>
2025-03-21 17:12:48 +0300

    changes

--- src/Middlewares/requestCountMiddleware.ts
+++ src/Middlewares/requestCountMiddleware.ts
@@ -1,13 +1,12 @@
 import { NextFunction, Request, Response } from "express"
 import { apiRequestCountCollection } from "../Repository/db";
 
-const firstRequestTimeMap = new Map<string, number>();
 
 
 export const requestCountMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const IP = req.ip;
     const URL = req.originalUrl;
-    const key = `${IP}-${URL}`; 
+    const key = `${IP}-${URL}`;
     const currentTime = new Date();
     const timeLimit = new Date(currentTime.getTime() - 10000)
 
@@ -26,28 +25,10 @@ export const requestCountMiddleware = async (req: Request, res: Response, next: 
 
 
     if (requestCount > 5) {
-        const firstRequestTime = firstRequestTimeMap.get(key) || currentTime.getTime()
-        const timeElapsed = currentTime.getTime() - firstRequestTime;
-
-        if (timeElapsed < 10000) {
-            // Ждем оставшееся время
-            const delay = 10000 - timeElapsed;
-            await new Promise((resolve) => setTimeout(resolve, delay));
-        }
-
-        // Сбрасываем время первого запроса
-        firstRequestTimeMap.set(key, currentTime.getTime());
-
-        // Возвращаем 429, если лимит превышен
         res.sendStatus(429);
         return;
     } else {
-        // Если это первый запрос, сохраняем время
-        if (!firstRequestTimeMap.has(key)) {
-            firstRequestTimeMap.set(key, currentTime.getTime());
-        }
 
-        // Продолжаем обработку запроса
         next();
     }
 };
