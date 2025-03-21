d2d409a6d3266893a3f9e6f6a716557319f59f33
Darya Matskevich <darramatskevich@gmail.com>
2025-03-21 23:46:06 +0300

    changes

--- src/Middlewares/requestCountMiddleware.ts
+++ src/Middlewares/requestCountMiddleware.ts
@@ -10,11 +10,11 @@ export const requestCountMiddleware = async (req: Request, res: Response, next: 
     const currentTime = new Date();
     const timeLimit = new Date(currentTime.getTime() - 10000)
 
-    await apiRequestCountCollection.deleteMany({
-        IP,
-        URL,
-        date: { $lt: timeLimit },
-    });
+    // await apiRequestCountCollection.deleteMany({
+    //     IP,
+    //     URL,
+    //     date: { $lt: timeLimit },
+    // });
 
     await apiRequestCountCollection.insertOne({ IP, URL, date: currentTime })
 
