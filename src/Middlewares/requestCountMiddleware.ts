80ded778ddda4e38f4b43e61c04b7264f753912b
Darya Matskevich <darramatskevich@gmail.com>
2025-03-23 15:11:56 +0300

    changes

--- src/Middlewares/requestCountMiddleware.ts
+++ src/Middlewares/requestCountMiddleware.ts
@@ -7,20 +7,22 @@ export const requestCountMiddleware = async (req: Request, res: Response, next: 
 
     const currentTime = new Date();
     const timeLimit = new Date(currentTime.getTime() - 10000)
-    await apiRequestCountCollection.deleteMany({
-        IP,
-        URL,
-        date: { $lt: timeLimit },
-    });
+ 
 
-    await apiRequestCountCollection.insertOne({ IP, URL, date: currentTime })
+    await apiRequestCountCollection.bulkWrite([
+        {deleteMany: {filter:{
+            IP,
+            URL,
+            date: { $lt: timeLimit } }}},
+        {insertOne: {document: { IP, URL, date: currentTime }}}
+        ])
 
     let requestCount = await apiRequestCountCollection.countDocuments({
         IP,
         URL,
         date: { $gte: timeLimit },
     })
-
+    
     if (requestCount > 5) {
         res.sendStatus(429);
         return;
