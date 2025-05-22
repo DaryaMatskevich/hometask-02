1531049675f712884a5e70544b0f17cbd06c1b87
Darya Matskevich <darramatskevich@gmail.com>
2025-05-22 15:08:33 +0300

    changes

--- src/queryRepository/likesPostQueryRepository.ts
+++ src/queryRepository/likesPostQueryRepository.ts
@@ -1,14 +1,19 @@
 import { ObjectId } from "mongodb";
 import { LikeStatusForPostModel} from "../Repository/db";
+import { isValidObjectId } from "mongoose";
 
 
 export class LikesPostQueryRepository {
 
     async getLikeStatusByUserId(userId: string, postId: string): Promise<string |  null> {
+        if (!isValidObjectId(userId) || !isValidObjectId(postId)) {
+    return null;
+}
+        
         const likeStatus = await LikeStatusForPostModel.findOne({
             userId: new ObjectId(userId),
             postId: new ObjectId(postId)
         }).lean()
-        return likeStatus && likeStatus.status ? likeStatus.status : null;
+        return likeStatus?.status || null;
     }
 }
